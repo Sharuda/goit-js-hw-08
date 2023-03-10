08537db2511d135364a3b10ddb819770b61bfd40
Sharuda <sharuda.yana98@gmail.com>
2023-03-10 13:06:30 +0200

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,28 +1,31 @@
-// const KEY_LOCALE_STORAGE = 'feedback-form-state';
-// const formEl = document.querySelector('.feedback-form');
-// const buttonSubmitEl = document.querySelector('[type = "submit"]');
+const KEY_LOCALE_STORAGE = 'feedback-form-state';
+const formEl = document.querySelector('.feedback-form');
+const buttonSubmitEl = document.querySelector('[type = "submit"]');
 
-// const { email, message } = formEl.elements;
+let formData = JSON.parse(localStorage.getItem(KEY_LOCALE_STORAGE)) || {};
+const { email, message } = formEl.elements;
 
-// formEl.addEventListener('input', handleInput);
-// buttonSubmitEl.addEventListener('submit', handleClickButtonSubmit);
+formEl.addEventListener('input', handleInput);
+buttonSubmitEl.addEventListener('submit', handleClickButtonSubmit);
 
-// populateTextInput();
+populateTextInput();
 
-// let formData = {};
+function handleInput() {
+  formData = { email: email.value, message: message.value };
+  localStorage.setItem(KEY_LOCALE_STORAGE, JSON.stringify(formData));
+}
 
-// function handleInput() {
-//   formData = {
-//     email: email.value,
-//     message: message.value,
-//   };
+function populateTextInput() {
+  if (formData) {
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+  }
+}
 
-//   localStorage.setItem(KEY_LOCALE_STORAGE, JSON.stringify(formData));
-// }
+function handleClickButtonSubmit(event) {
+  event.preventDefault();
+  console.log({ email: email.value, message: message.value });
 
-// function populateTextInput() {}
-
-// function handleClickButtonSubmit(event) {
-//   event.preventDefault();
-//   console.log(JSON.parse(formData));
-// }
+  event.target.reset();
+  localStorage.removeItem(KEY_LOCALE_STORAGE);
+}
