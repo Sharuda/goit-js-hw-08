b0e120af9cb39b75b3fa0fc6322b0347b29dbcb4
Sharuda <sharuda.yana98@gmail.com>
2023-03-10 12:43:15 +0200

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+// const KEY_LOCALE_STORAGE = 'feedback-form-state';
+// const formEl = document.querySelector('.feedback-form');
+// const buttonSubmitEl = document.querySelector('[type = "submit"]');
 
+// const { email, message } = formEl.elements;
+
+// formEl.addEventListener('input', handleInput);
+// buttonSubmitEl.addEventListener('submit', handleClickButtonSubmit);
+
+// populateTextInput();
+
+// let formData = {};
+
+// function handleInput() {
+//   formData = {
+//     email: email.value,
+//     message: message.value,
+//   };
+
+//   localStorage.setItem(KEY_LOCALE_STORAGE, JSON.stringify(formData));
+// }
+
+// function populateTextInput() {}
+
+// function handleClickButtonSubmit(event) {
+//   event.preventDefault();
+//   console.log(JSON.parse(formData));
+// }
