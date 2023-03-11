ad8d6c661bae64356d3ee944173ecd4cdbf675eb
Sharuda <sharuda.yana98@gmail.com>
2023-03-11 12:29:34 +0200

    change task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,18 +12,4 @@ const onPlay = function (data) {
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player
-  .setCurrentTime(CURRENT_TIME)
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)) || 0);
